d8c1f281b90e3a1b67394dd39da8acf559e8854b
Cristiano José Cecanho <cecanho@gmail.com>
2021-12-02 09:12:38 -0300

    Update LimpaTemp.bat
    Padronização dos parâmetros dos comandos DOS

--- LimpaTemp.bat
+++ LimpaTemp.bat
@@ -1,10 +1,10 @@
-DEL /F /S /Q %HOMEPATH%\Config~1\Temp\*.* 
-DEL /F /S /Q C:\WINDOWS\Temp\*.* 
-DEL /F /S /Q C:\WINDOWS\Prefetch\*.* 
+DEL %HOMEPATH%\Config~1\Temp\*.* /F /S /Q
+DEL C:\WINDOWS\Temp\*.* /F /S /Q
+DEL C:\WINDOWS\Prefetch\*.* /F /S /Q
 DEL "%WINDIR%\Tempor~1\*.*" /F /S /Q 
-RD /S /Q "%HOMEPATH%\Config~1\Temp" 
+RD "%HOMEPATH%\Config~1\Temp" /S /Q
 MD "%HOMEPATH%\Config~1\Temp" 
-RD /S /Q C:\WINDOWS\Temp\ 
+RD C:\WINDOWS\Temp\ /S /Q
 MD C:\WINDOWS\Temp 
-RD /S /Q C:\WINDOWS\Prefetch\ 
+RD C:\WINDOWS\Prefetch\ /S /Q
 MD C:\WINDOWS\Prefetch
